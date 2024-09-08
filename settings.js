b0e1bc05a1c46dceac1e253855d2fde08038c8c9
Arslan7861291 <113829547+Arslan7861291@users.noreply.github.com>
2024-09-08 11:24:15 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '923200199895' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=OBdwwQBZ#xRKqq8CyYhMw3g_w01aMZVDuTOSww1bKqtNrjNgdg74' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=PEcSXQJJ#UHLCmmTumYFKHMYW3dBFPAh25HeT9tqiv7x7xCoohwA' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "923415404892,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
